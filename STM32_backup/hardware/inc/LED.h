0e3173fb4839e89b9219689d74374763a8047e45
abc <3424445365@qq.com>
2026-05-31 17:15:17 +0800

    删除了STM32文件夹，添加了AT32F403A文件夹，并将STM32文件夹中的内容移动到AT32F403A文件夹中。

--- STM32_backup/hardware/inc/LED.h
+++
@@ -1,9 +0,0 @@
-#ifndef _LED_H
-#define _LED_H
-
-void LED_Init(void);
-void LED_ON(void);
-void LED_OFF(void);
-void LED_Turn(void);
-
-#endif
--- STM32_backup/user/NVIC.h
+++
@@ -1,7 +0,0 @@
-#ifndef _NVIC_H
-#define _NVIC_H
-
-void NVIC_Configuration(void);
-
-
-#endif
